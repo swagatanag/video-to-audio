a89762cdbc9ee9b9d3d5e840936a67b256938f9f
sush19mita <sush2004mita@gmail.com>
2025-10-03 10:37:35 +0530

    Fix import for MoviePy 2.x

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from moviepy.editor import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
 import os
 
 st.set_page_config(page_title="Video to Audio Extractor", page_icon="🎵")
